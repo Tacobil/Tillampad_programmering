6002df88fbf18a4c4cdc21f905eac6a121d402a0
Tacobil <Tacobil123@gmail.com>
2026-01-17 10:34:31 +0100

    Inlämning 2

--- project_2/project_2.ino
+++ project_2/project_2.ino
@@ -1,6 +1,13 @@
-  /*
- https://www.youtube.com/watch?v=nW5FUVzYCKM
+/* 
+  Name: 2D-Drawer
+  Author: Simon Bukvic
+  Date: 2026-01-14
+  Description: 
 */
+
+
+// https://www.youtube.com/watch?v=nW5FUVzYCKM
+
 
 #include <Servo.h>
 
@@ -10,39 +17,36 @@ Servo servoLift;
 
 // Units in cm
 // 0.8 cm between each stud for lego
-#define R1 4.8 // bottom right arm
-#define R2 4.8 // top right arm
+#define R1 4.8  // bottom right arm
+#define R2 4.8  // top right arm
 
-#define L1 4.8 // bottom left arm
-#define L2 6.4 // top left arm
+#define L1 4.8  // bottom left arm
+#define L2 6.4  // top left arm
 
-#define OFFSET 2.8 // distance between left servo and right servo
-#define PENOFFSET 1.6 // distance between top joint and pen
+#define OFFSET 2.8     // distance between left servo and right servo
+#define PENOFFSET 1.6  // distance between top joint and pen
 
 // pins
 #define LEFTPIN 9
 #define RIGHTPIN 10
 #define LIFTPIN 11
-#define CALIBRATEPIN 2 // when pin is high, servos are set to 90 degrees, making it easy to calibrate
+#define CALIBRATEPIN 2  // when pin is high, servos are set to 90 degrees, making it easy to calibrate
 
 #define PI 3.141528
 
 
-String inputString = "";   // stores incoming chars
-int servoAngle = 90;
+String inputString = "";  // stores incoming chars
 float lastX = 0;
 float lastY = 0;
 
 void setPos(double x, double y, bool debug = false) {
-  outOfReach(x, y);
-
   y = -y;
   // left arm inverse kinematics
-  double q2Left = -acos((x*x + y*y - L1*L1 - L2*L2) / (2 * L1 * L2));
+  double q2Left = -acos((x * x + y * y - L1 * L1 - L2 * L2) / (2 * L1 * L2));
   double q1Left = atan2(y, x) + atan2((L2 * sin(q2Left)), (L1 + L2 * cos(q2Left)));
   q2Left = -q2Left;
-  
-  
+
+
   // compensate servo offset
   x -= OFFSET;
 
@@ -51,63 +55,111 @@ void setPos(double x, double y, bool debug = false) {
   y -= sin(q1Left + q2Left) * PENOFFSET;
 
   // right arm inverse kinematics
-  double q2Right = acos((x*x + y*y - R1*R1 - R2*R2) / (2 * R1 * R2));
+  double q2Right = acos((x * x + y * y - R1 * R1 - R2 * R2) / (2 * R1 * R2));
   double q1Right = atan2(y, x) + atan2((R2 * sin(q2Right)), (R1 + R2 * cos(q2Right)));
 
   q1Left = -q1Left;
   q1Right = -q1Right;
 
-  int angleLeftMillis = map(q1Left*1000, 0, PI*1000, 544, 2400);
-  int angleRightMillis = map(q1Right*1000, 0, PI*1000, 544, 2400);
+  int angleLeftMillis = map(q1Left * 1000, 0, PI * 1000, 544, 2400);
+  int angleRightMillis = map(q1Right * 1000, 0, PI * 1000, 544, 2400);
   servoLeft.writeMicroseconds(angleLeftMillis);
   servoRight.writeMicroseconds(angleRightMillis);
-  // int angleLeft = q1Left * 180 / PI;
-  // int angleRight = q1Right * 180 / PI;
-  // servoLeft.write(angleLeft);
-  // servoRight.write(angleRight);
- 
-  if (debug) {
-  Serial.println("angle left: "+String(angleLeftMillis)+", "+String(q1Left)+", "+String(q2Left));
-  Serial.println("angle right: "+String(angleRightMillis)+", "+String(q1Right)+", "+String(q2Right));
-  }
 
+  if (debug) {
+    Serial.println("angle left: " + String(angleLeftMillis) + ", " + String(q1Left) + ", " + String(q2Left));
+    Serial.println("angle right: " + String(angleRightMillis) + ", " + String(q1Right) + ", " + String(q2Right));
+  }
 }
 
 void setup() {
   servoLeft.attach(LEFTPIN);
   servoRight.attach(RIGHTPIN);
   servoLift.attach(LIFTPIN);
-  
+
   Serial.begin(9600);
-  servosWrite(90);
+  servoLeft.write(90);
+  servoRight.write(90);
 
   pinMode(CALIBRATEPIN, INPUT);
 }
 
-void servosWrite(int angle) {
-  servoLeft.write(angle);
-  servoRight.write(angle);
-  servoLift.write(angle);
-}
+/* 
+Function that handles serial monitor messages.
 
-// function that allows you to message an angle to set all servos to. 
-void inputServoUpdate() {
+COMMANDS:
+  a (n) - set servo to angle n. if no angle is specified, sets servo angles to 90 degrees
+  c (x, y, r) - draw circle at position (x, y) with radius r
+  r (x y w h) - draw rect at position (x, y) with width w and height h
+  p (x y) - set position to (x, y)
+
+  m (x, y, text) - text at position (x, y)
+  n (x, y, text) - number at position (x, y)
+
+  example:
+    c 3 5 4 - draw circle at position (3, 5) with radius 4
+
+*/
+void handeInput() {
   while (Serial.available() > 0) {
     char c = Serial.read();
 
     if (c == '\n' || c == '\r') {
-      // We reached the end of the input line → convert to number
-      if (inputString.length() > 0) {
-        Serial.println("input: "+inputString);
-        int angle = inputString.toInt();
-        servoAngle = angle;
-        // servosWrite(angle);
-        
-        Serial.print("Set servo to: ");
-        Serial.println(angle);
+            
+      char keyword = inputString[0];
+      String rest = inputString.substring(2);
 
-        inputString = ""; // clear for next command
-      }
+      Serial.println("keyword: " + String(keyword));
+
+      switch (keyword) {
+        case 'q': // reset
+          Serial.println("reset");
+          servoLeft.write(90);
+          servoRight.write(90);
+          break;
+
+        case 'a': // angle
+          Serial.println("angle");
+          Serial.println("Set servo angles to " + String(rest.toInt()) + " degrees");
+          servoLeft.write(rest.toInt());
+          servoRight.write(rest.toInt());
+          break;
+
+        case 'c': // circle
+          Serial.println("Circle");
+          drawCircle(OFFSET / 2, 8, 2);
+          break;
+
+        case 'r': // rect
+          Serial.println("Rectangle");
+          drawRect(OFFSET/2, 7, 2, 2);
+          break;
+        
+        case 'p': // set position
+          Serial.println("Set position to " + String(rest[0]) + "," + String(rest[2]));
+          setPos(String(rest[0]).toInt(), String(rest[2]).toInt());
+          break;
+
+        case 'm': // message
+          Serial.println("message");
+          //
+          break;
+        
+        case 'n': // number
+          Serial.println("number");
+          break;
+        
+        case 'l': // line
+          Serial.println("line");
+          setPos(0,5);
+          lastX = 0;
+          lastY = 5;
+          drawTo(5,5);
+          break;
+      }        
+
+      inputString = "";  // clear for next command
+      
     } else {
       // build the string
       inputString += c;
@@ -116,84 +168,56 @@ void inputServoUpdate() {
 }
 
 void loop() {
-  if (digitalRead(CALIBRATEPIN) == HIGH) {
-    inputServoUpdate();
-    servosWrite(servoAngle);
-    // setPos(OFFSET/2, 7);
-  } else {
-    drawCircle(OFFSET/2, 8, 2);
-    // followXAxis(4, 7);
-    // drawSquare(OFFSET/2, 7, 3, 3);
-    // setPos(OFFSET/2, 7);
-  }
-}
-
-void followXAxis(float distance, float y) {
-  float x = OFFSET / 2;
-  float inc = 0.05;
-  float start = (OFFSET - distance) / 2;
-  float stop = (OFFSET + distance) / 2;
-  
-  setPos(start, y);
-  delay(1000);
-
-  for (float x = start; x <= stop; x+=inc) {
-    setPos(x, y);
-    delay(10);
-  }
-
-  delay(1000);
+  handeInput();
+  delay(100);
 }
 
 void drawTo(float x, float y) {
-  for (int i = 0; i <= 100; i++) {
+  int steps = 200;
+  double stepX = (x - lastX) / steps;
+  double stepY = (y - lastY) / steps;
+  
+  Serial.println("Drawing line from (" + String(lastX) + "," + String(lastY) + ") to (" + String(x) + "," + String(y) + ")");
 
-    delay(2);
+  for (int i = 0; i <= steps; i++) {
+    Serial.println(String(stepX * i + lastX) + " - " + String(stepY * i + lastY)); 
+    setPos(stepX * i + lastX, stepY * i + lastY);
+    delay(5);
   }
+
+  setPos(x, y);
+
   lastX = x;
   lastY = y;
 }
 
-void drawSquare(float x, float y, float w, float h) {
-  // top side  
-  float step = 0.05;
-  int d = 6;
-
-  for (float i = x - w/2; i <= x + w/2; i += step) {
-    setPos(i, y + h/2);
-    delay(d);
+void lift(int p) {
+  switch (p) {
+    case 0: // pen down
+      servoLift.write(90);
+      break;
+    case 1: // pen up
+      servoLift.write(100);
+      break;
+    case 2: // pen idle
+      servoLift.write(110);
+      break;
   }
-  setPos(x+w/2, y+h/2);
+}
 
-  // right side
-  for (float i = y + h/2; i >= y - h/2; i -= step) {
-    setPos(x + w/2, i);
-    delay(d);
-  }
-  setPos(x+w/2, y-h/2);
-
-  // bottom side
-  for (float i = x + w/2; i >= x - w/2; i -= step) {
-    setPos(i, y - h/2);
-    delay(d);
-  }
-  setPos(x-w/2, y-h/2);
-
-  // left side
-  for (float i = y - h/2; i <= y + h/2; i += step) {
-    setPos(x - w/2, i);
-    delay(d);
-  }
-  setPos(x-w/2, y+h/2);
-
+void drawRect(float x, float y, float w, float h) {
+  drawTo(x - w / 2, y + h / 2);
+  drawTo(x + w / 2, y + h / 2);
+  drawTo(x - w / 2, y - h / 2);
+  drawTo(x + w / 2, y - h / 2);
 }
 
 void drawCircle(float x, float y, float r) {
-  for (float t = 0; t <= 2*PI; t += 0.005) {
+  for (float t = 0; t <= 2 * PI; t += 0.05) {
     float px = x + r * cos(t);
     float py = y + r * sin(t);
     setPos(px, py);
-    delay(2);
+    delay(10);
   }
 
   // close circle
@@ -201,17 +225,14 @@ void drawCircle(float x, float y, float r) {
 }
 
 bool outOfReach(double x, double y) {
-  if (x*x + y*y >= L1*L1 + L2*L2) {
+  if (x * x + y * y >= L1 * L1 + L2 * L2) {
     //Serial.println("left out of reach");
     return true;
   }
 
-  if ((x-OFFSET)*(x-OFFSET) + y*y >= R1*R1 + R2*R2) {
+  if ((x - OFFSET) * (x - OFFSET) + y * y >= R1 * R1 + R2 * R2) {
     //Serial.println("right out of reach");
     return true;
   }
   return false;
 }
-
-
-  
